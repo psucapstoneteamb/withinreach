daa1073e41b7e258aeef0afd0c0d63408bf8729b
Clinton Olson <clint.olson2@gmail.com>
2013-06-15 18:15:50 -0700

    test commit

--- WithinReach/src/org/leifolson/withinreach/MenuActivity.java
+++ WithinReach/src/org/leifolson/withinreach/MenuActivity.java
@@ -2,6 +2,7 @@ package org.leifolson.withinreach;
 
 import android.support.v4.app.FragmentActivity;
 
+// adding as a test to see if git repo manager picks this up
 public class MenuActivity extends FragmentActivity {
 
 }

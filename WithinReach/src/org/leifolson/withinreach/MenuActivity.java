53001d6c54986ce0f590dce3c4abc528b2b2bbfb
Clinton Olson <clint.olson2@gmail.com>
2013-06-15 18:06:47 -0700

    Adding WithinReach skeleton file structure to the repo

---
+++ WithinReach/src/org/leifolson/withinreach/MenuActivity.java
@@ -0,0 +1,7 @@
+package org.leifolson.withinreach;
+
+import android.support.v4.app.FragmentActivity;
+
+public class MenuActivity extends FragmentActivity {
+
+}
---
+++ WithinReach/src/org/leifolson/withinreach/ServerComMgr.java
@@ -0,0 +1,5 @@
+package org.leifolson.withinreach;
+
+public class ServerComMgr {
+
+}

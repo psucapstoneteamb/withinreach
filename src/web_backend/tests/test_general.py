4b5aa82454b04646413064ba5bfecc6c366f3fde
hanrong <hanrong@pdx.edu>
2013-07-14 16:18:48 -0700

    testcase for controller

--- src/web_backend/tests/test_general.py
+++ src/web_backend/tests/test_general.py
@@ -3,6 +3,7 @@ import unittest
 
 # Local
 from webapp import create_app
+from webapp.controller import Controller
 
 class BaseTestCase(unittest.TestCase):
 
@@ -14,14 +15,27 @@ class BaseTestCase(unittest.TestCase):
         create_app("test")
         from webapp import app #, db
         self.c = app.test_client() # or only app?
+        
+        
 
     def tearDown(self):
         #os.close(self.db_fd)
         #os.unlink(flaskr.app.config['DATABASE'])
         pass
     
-    def test_simple(self):
+    def test_case1(self):
         self.assertEqual(1, 1)
         self.assertEqual(self.c.get('/')._status_code,302)
         
-
+    def test_case2(self):
+        request_args = {'lat':100.55,
+                        'long': 200.66,
+                        'time': 45,
+                        'day': 20,
+                        'month': 6,
+                        'year': 2010,
+                        'mode_code': 1,
+                        'constraint': 45}
+        ctr = Controller(request_args)
+        val = ctr.validate()
+        self.assertEqual(val,True)

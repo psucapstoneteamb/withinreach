04c4c63cffd1bf4e0a79d568fc1c50251a40beba
viredfox <viredfox@gmail.com>
2013-08-05 22:39:07 -0700

    trivial

--- src/web_backend/run_tests.py
+++ src/web_backend/run_tests.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == '__main__':
     import nose
     argv = sys.argv[:]
-    #argv.insert(1, '--nocapture')
+    #argv.insert(1, '--nocapture') # nose is sneaky with the 1st arg
     nose.main(argv=argv)
